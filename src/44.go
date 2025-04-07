573d5fc8ea169aed3fc4bb712524d39fd579f0f7
rajaphoumypippen868 <rajaphoumypippen868@gmail.com>
2025-04-07 15:10:31 +0000

    Add 44.go

---
+++ src/44.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Example Go code
+    var i int = 5
+    if i > 3 {
+        fmt.Println("i is greater than 3")
+    } else {
+        fmt.Println("i is not greater than 3")
+    }
+}
