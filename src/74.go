df8d86216f41a4c8fa8897a025c15a9e060d01cd
rajaphoumypippen868 <rajaphoumypippen868@gmail.com>
2025-04-07 15:52:44 +0000

    Add 74.go

---
+++ src/74.go
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class GoCodeGenerator {
+    public static String generateGoCode() {
+        // Example of a simple "Hello World" program in Go
+        return "package main\n";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateGoCode());
+    }
+}
