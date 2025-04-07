ebf0caed88e29435870bfaa007fd11ad6ecc9892
rajaphoumypippen868 <rajaphoumypippen868@gmail.com>
2025-04-07 14:56:45 +0000

    Add 34.go

---
+++ src/34.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int = 5
+    if a > 10 {
+        fmt.Println("a is greater than 10")
+    } else {
+        fmt.Println("a is not greater than 10")
+    }
+}
