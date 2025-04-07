8f6aa6406ec7358ebcf151a56b2d03d825445b97
rajaphoumypippen868 <rajaphoumypippen868@gmail.com>
2025-04-07 15:01:37 +0000

    Add 38.go

---
+++ src/38.go
@@ -0,0 +1,7 @@
+public class GoProblemGenerator {
+    public static void main(String[] args) {
+        // Generate a random integer between 1 and 99
+        int randomNumber = (int)(Math.random() * 90);
+        System.out.println("The generated number is: " + randomNumber);
+    }
+}
